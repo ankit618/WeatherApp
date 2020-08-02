b18b74800fb0125bbf7fe68a81fcf25ab0d9291c
Ankit Shukla <37836105+ankit618@users.noreply.github.com>
2020-08-02 21:47:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,25 +22,25 @@ function searchWeather(searchTerm) {
 function init(resultFromServer) {
     switch (resultFromServer.weather[0].main) {
         case 'Clear':
-            document.body.style.backgroundImage = "url('clear.jpg')";
+            document.body.style.backgroundImage = "url('Clear.jpg')";
             break;
         
         case 'Clouds':
-            document.body.style.backgroundImage = "url('cloudy.jpg')";
+            document.body.style.backgroundImage = "url('Cloudy.jpg')";
             break;
 
         case 'Rain':
         case 'Drizzle':
         case 'Mist':
-            document.body.style.backgroundImage = "url('rain.jpg')";
+            document.body.style.backgroundImage = "url('Rain.jpg')";
             break;
         
         case 'Thunderstorm':
-            document.body.style.backgroundImage = "url('storm.jpg')";
+            document.body.style.backgroundImage = "url('Storm.jpg')";
             break;
         
         case 'Snow':
-            document.body.style.backgroundImage = "url('snow.jpg')";
+            document.body.style.backgroundImage = "url('Snow.jpg')";
             break;
 
         default:
@@ -54,7 +54,7 @@ function init(resultFromServer) {
     let cityHeader = document.getElementById('cityHeader');
 
     let weatherIcon = document.getElementById('documentIconImg');
-    weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
+    weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].Icon + '.png';
 
     let resultDescription = resultFromServer.weather[0].description;
     weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
