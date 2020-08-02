2d621427a81c18be1a392b3c0f5d0a570976e88f
Ankit Shukla <37836105+ankit618@users.noreply.github.com>
2020-08-02 21:56:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function searchWeather(searchTerm) {
     fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
         .then((result) => {
             return result.json();
-        }).then((res) => {
-            init(res);
+        }).then((result) => {
+            init(result);
     });
 }
 
