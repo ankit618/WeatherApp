8518275ebd9aa0b2d3a8a531c83cc14097f69443
Ankit Shukla <37836105+ankit618@users.noreply.github.com>
2020-08-09 17:15:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function getSearchMethod(searchTerm) {
 
 function searchWeather(searchTerm) {
     getSearchMethod(searchTerm);
-    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
         .then((result) => {
             return result.json();
         }).then((res) => {
@@ -55,7 +55,7 @@ function init(resultFromServer) {
     let cityHeader = document.getElementById('cityHeader');
 
     let weatherIcon = document.getElementById('documentIconImg');
-    weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].Icon + '.png';
+    weatherIcon.src = 'https://openweathermap.org/img/w/' + resultFromServer.weather[0].Icon + '.png';
 
     let resultDescription = resultFromServer.weather[0].description;
     weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
