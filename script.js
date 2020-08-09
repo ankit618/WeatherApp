c40cef0f933d4e6893e7615c26ceb830ab88e841
Ankit Shukla <37836105+ankit618@users.noreply.github.com>
2020-08-09 18:02:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let appId = '1cb2bceb407bf15975924fc09e39af0d';
+
 let units = 'metric'; // other option is metric
 let searchMethod; // q means searching as a string.
 
@@ -22,27 +23,28 @@ function searchWeather(searchTerm) {
 function init(resultFromServer) {
     switch (resultFromServer.weather[0].main) {
         case 'Clear':
-
-            document.body.style.backgroundImage = "url('Clear.jpg')";
+            document.body.style.backgroundImage = "url('clear.jpg')";
             break;
         
         case 'Clouds':
-            case 'Haze':    
-            document.body.style.backgroundImage = "url('Cloudy.jpg')";
+            document.body.style.backgroundImage = "url('cloudy.jpg')";
             break;
+        case 'Haze':
+            document.body.style.backgroundImage = "url('haze.jpg')";
+            break;    
 
         case 'Rain':
         case 'Drizzle':
         case 'Mist':
-            document.body.style.backgroundImage = "url('Rain.jpg')";
+            document.body.style.backgroundImage = "url('rain.jpg')";
             break;
         
         case 'Thunderstorm':
-            document.body.style.backgroundImage = "url('Storm.jpg')";
+            document.body.style.backgroundImage = "url('storm.jpg')";
             break;
         
         case 'Snow':
-            document.body.style.backgroundImage = "url('Snow.jpg')";
+            document.body.style.backgroundImage = "url('snow.jpg')";
             break;
 
         default:
@@ -56,7 +58,7 @@ function init(resultFromServer) {
     let cityHeader = document.getElementById('cityHeader');
 
     let weatherIcon = document.getElementById('documentIconImg');
-    weatherIcon.src = 'https://openweathermap.org/img/w/' + resultFromServer.weather[0].Icon + '.png';
+    weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
 
     let resultDescription = resultFromServer.weather[0].description;
     weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
@@ -83,3 +85,10 @@ document.getElementById('searchBtn').addEventListener('click', () => {
     if(searchTerm)
         searchWeather(searchTerm);
 });
+
+// DEFAULT https://www.pexels.com/photo/water-blue-ocean-5412/
+// RAIN https://www.pexels.com/photo/road-landscape-nature-forest-39811/
+// CLOUDS https://www.pexels.com/photo/grey-white-clouds-158163/
+// SNOW https://www.pexels.com/photo/abstract-art-background-blue-sky-289649/
+// CLEAR https://www.pexels.com/photo/blue-sky-blur-clear-sky-color-281260/
+// STORM https://www.pexels.com/photo/white-lightning-heating-mountain-53459/
